dc8bf998ebb83fe28cd4dd16e3fe91463879a41a
Nico Tuban (CW) <ntuban@paypalcorp.com>
2022-06-04 18:26:15 +0800

    Building databse and database queries

--- dev_apps/views.py
+++ dev_apps/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from dev_apps.models import Project
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'dev_apps/projects.html', context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    context = {'project': projectObj, 'tags': tags}
+    return render(request, 'dev_apps/single-project.html', context)
