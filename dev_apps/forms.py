2fa5205cd85218d3d4e41001ace9d53f47dc3222
Nico Tuban (CW) <ntuban@paypalcorp.com>
2022-06-07 16:47:15 +0800

    Integrating the theme

--- dev_apps/forms.py
+++ dev_apps/forms.py
@@ -1,8 +1,23 @@
 from dataclasses import field
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'feature_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs) #overide the class attribute of the fields
+
+        #looping all fields to apply the class input style or attributes
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+
+
+        #instead of doing this one by one we gonna loop this instead.
+       # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})# changing class into input style
--- devsearch/settings.py
+++ devsearch/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--s)2w=1o3vre$48qb65(qukygo4tzd&pkd9!7z#uk)!va@jp)f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
