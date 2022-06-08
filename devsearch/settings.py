698bd5170d5c88456a515b498019107827f5efad
Nico Tuban (CW) <ntuban@paypalcorp.com>
2022-06-08 09:14:40 +0800

    Added User App

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dev_apps',
+    'users',
 ]
 
 MIDDLEWARE = [
--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,14 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dev_apps.urls')),
+    path('projects/', include('dev_apps.urls')),
+    path('', include('users.urls')),
 
 ]
 
