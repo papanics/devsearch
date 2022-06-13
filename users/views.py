369340c05ffdf36846f0cd4620db29e08ec3c971
Nico Tuban (CW) <ntuban@paypalcorp.com>
2022-06-13 13:43:14 +0800

    Created User Registration

--- users/views.py
+++ users/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -34,7 +36,31 @@ def logoutUser(request):
     messages.error(request, 'User was Logged out')
     return redirect('login')
 
-# Create your views here.
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User Account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+
+        else: 
+            messages.error(request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
+
+
+
+
 def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
