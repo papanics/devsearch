17f5274e6282330fe62026ce2dc0b7ee9293c26b
Nico Tuban (CW) <ntuban@paypalcorp.com>
2022-06-15 20:38:09 +0800

    User Project CRUD

--- dev_apps/views.py
+++ dev_apps/views.py
@@ -18,12 +18,15 @@ def project(request, pk):
 
 @login_required(login_url="login") # if the user not login, send the user to the login page.
 def createProject(request):
+    profile = request.user.profile # 1.took the current loggin user
     form = ProjectForm()
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile # 2. associating with the project owner
+            project.save()
             return redirect('projects')
 
     context = {'form': form}
@@ -31,7 +34,8 @@ def createProject(request):
 
 @login_required(login_url="login")
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile #only the owner can update his project
+    project = profile.project_set.get(id=pk) # getting all the projects of that specific user
     form = ProjectForm(instance=project)
 
     if request.method == 'POST':
@@ -45,7 +49,8 @@ def updateProject(request, pk):
 
 @login_required(login_url="login")
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk) # only the owner of the project can delete on his own project
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
