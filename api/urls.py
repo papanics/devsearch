4389647c319d1678cb321aaabdca9bebdf4b06db
Nico Tuban (CW) <ntuban@paypalcorp.com>
2022-06-26 14:55:39 +0800

    Authentication With JSON Web Tokens

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, # generate a token based on the user.
+    TokenRefreshView, # generate a refresh toolkit.
+)
+
 
 urlpatterns = [
+
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #basically anytime the token expires, it will send a refresh token to generate a new token.
+
     path('', views.getRoutes),
     path('projects/', views.getProjects),
     path('projects/<str:pk>', views.getProject),
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from dev_apps.models import Project
@@ -21,6 +22,7 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+#@permission_classes([IsAuthenticated]) # Restriction  route --> only the authenticated user can view the projects
 def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
