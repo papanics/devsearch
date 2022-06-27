c53099ae98375f453486518efffcbbad0d4e9dcf
Nico Tuban (CW) <ntuban@paypalcorp.com>
2022-06-27 09:10:13 +0800

    Sending POST Requests

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from dev_apps.models import Project
+from dev_apps.models import Project, Review
 
 
 @api_view(['GET'])
@@ -32,6 +32,27 @@ def getProjects(request):
 @api_view(['GET'])
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+#sending post request
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.object.get(id=pk)
+    user = request.user.profile # remember that the user is no longer in the session, it is in the token.
+    data = request.data
+
+    review, created = Review.objects.get_or_create( #if the user already has a review on that project then it will get or return that user else create a review.
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
 
